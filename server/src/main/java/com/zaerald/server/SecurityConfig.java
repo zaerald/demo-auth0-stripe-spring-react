8c865f0ba978bb2f8d2eec02da1784f91c3e6516
zaerald <zaerald.zd@gmail.com>
2023-02-16 21:41:43 +0800

    feat: add security configuration

---
+++ server/src/main/java/com/zaerald/server/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.zaerald.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
+    }
+
+}
