126d517bb48b0756e21f0b2a46e1d8cb7ee3acab
zaerald <zaerald.zd@gmail.com>
2023-02-17 00:23:02 +0800

    feat(client): consume api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,11 @@ type Message = {
 function App() {
   const [message, setMessage] = useState("");
 
+  const [privateMessage, setPrivateMessage] = useState("");
+
   useEffect(() => {
     (async () => {
-      const response = await fetch("http://localhost:8080/api/hello");
+      const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/api/public`);
       const data = await response.json();
 
       if ("content" in data && typeof data.content === "string") {
@@ -20,7 +22,27 @@ function App() {
     })();
   }, []);
 
-  return <div>Message: {message}</div>;
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${import.meta.env.VITE_BASE_API_URL}/api/private`, {
+        credentials: "include",
+      });
+      const data = await response.json();
+
+      if ("content" in data && typeof data.content === "string") {
+        const messageData = data as Message;
+        setPrivateMessage(messageData.content);
+      }
+    })();
+  }, []);
+
+  return (
+    <div>
+      <a href={`${import.meta.env.VITE_BASE_API_URL}/oauth2/authorization/auth0`}>Login</a>
+      <p>Message: {message}</p>
+      <p>Private Message: {privateMessage ?? "None"}</p>
+    </div>
+  );
 }
 
 export default App;
