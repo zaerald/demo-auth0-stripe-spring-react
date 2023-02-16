67615ca7e874458703aa714f910be61cca59cb20
zaerald <zaerald.zd@gmail.com>
2023-02-16 22:55:56 +0800

    feat(server): implement auth0 integration

--- server/src/main/java/com/zaerald/server/SecurityConfig.java
+++ server/src/main/java/com/zaerald/server/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.zaerald.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,7 +13,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/api/public").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().oauth2Login();
         return http.build();
     }
 
