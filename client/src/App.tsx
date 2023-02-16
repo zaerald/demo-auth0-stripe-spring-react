e703f9b1e1df7226d7866b02721fefa415ecbba2
zaerald <zaerald.zd@gmail.com>
2023-02-16 21:54:56 +0800

    feat: perform a call and allow CORS origin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+type Message = {
+  content: string;
+};
+
 function App() {
-  return <div>Message</div>;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://localhost:8080/api/hello");
+      const data = await response.json();
+
+      if ("content" in data && typeof data.content === "string") {
+        const messageData = data as Message;
+        setMessage(messageData.content);
+      }
+    })();
+  }, []);
+
+  return <div>Message: {message}</div>;
 }
 
 export default App;
--- server/src/main/java/com/zaerald/server/HelloController.java
+++ server/src/main/java/com/zaerald/server/HelloController.java
@@ -1,11 +1,14 @@
 package com.zaerald.server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+// for demonstration purposes, allow origin for everything
+@CrossOrigin(origins = "*")
 public class HelloController {
 
    @GetMapping("/hello")
